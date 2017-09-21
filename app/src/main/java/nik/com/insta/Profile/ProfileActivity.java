fad61a174ec16a27851e6801fd7c14146e9df9a9
Nik <ravipatel84184@gmail.com>
2017-09-21 19:28:37 +0530

    21-9-2017

--- app/src/main/java/nik/com/insta/Profile/ProfileActivity.java
+++ app/src/main/java/nik/com/insta/Profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package nik.com.insta.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
@@ -8,10 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import nik.com.insta.R;
 import nik.com.insta.Utils.ViewCommentsFragment;
 import nik.com.insta.Utils.ViewPostFragment;
+import nik.com.insta.Utils.ViewProfileFragment;
 import nik.com.insta.models.Photo;
 
 /**
@@ -82,11 +85,34 @@ public class ProfileActivity extends AppCompatActivity implements
     private void init(){
         Log.d(TAG, "init: inflating " + getString(R.string.profile_fragment));
 
-        ProfileFragment fragment = new ProfileFragment();
-        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(getString(R.string.profile_fragment));
-        transaction.commit();
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(getString(R.string.calling_activity))){
+            Log.d(TAG, "init: searching for user object atteched as intent extra");
+
+            if (intent.hasExtra(getString(R.string.intent_user))){
+                Log.d(TAG, "init: inflating view profile");
+                ViewProfileFragment fragment = new ViewProfileFragment();
+                Bundle args = new Bundle();
+                args.putParcelable(getString(R.string.intent_user),
+                        intent.getParcelableExtra(getString(R.string.intent_user)));
+                fragment.setArguments(args);
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container,fragment);
+                transaction.addToBackStack(getString(R.string.view_profile_fragment));
+                transaction.commit();
+            }else {
+                Toast.makeText(mContext, "something went wrong ", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Log.d(TAG, "init: inflating Profile");
+            ProfileFragment fragment = new ProfileFragment();
+            FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.container, fragment);
+            transaction.addToBackStack(getString(R.string.profile_fragment));
+            transaction.commit();
+        }
     }
 
 
--- app/src/main/java/nik/com/insta/Search/SearchActivity.java
+++ app/src/main/java/nik/com/insta/Search/SearchActivity.java
@@ -1,18 +1,35 @@
 package nik.com.insta.Search;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import nik.com.insta.Profile.ProfileActivity;
 import nik.com.insta.R;
 import nik.com.insta.Utils.BottomNavigationViewHelper;
-
+import nik.com.insta.Utils.UserListAdapter;
+import nik.com.insta.models.User;
+import java.util.*;
 /**
  * Created by User on 5/28/2017.
  */
@@ -23,13 +40,115 @@ public class SearchActivity extends AppCompatActivity{
 
     private Context mContext = SearchActivity.this;
 
+    //widgets
+    private EditText mSearchParam;
+    private ListView mListView;
+
+
+    //vars
+    private  List<User> mUserList;
+    private UserListAdapter mAdapter;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate: started.");
 
+        mSearchParam = (EditText) findViewById(R.id.search);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        hideSoftKeyboard();
         setupBottomNavigationView();
+        initTextListener();
+    }
+
+    private void initTextListener(){
+        Log.d(TAG, "initTextListener: initializing ");
+
+        mUserList = new ArrayList<>();
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = mSearchParam.getText().toString().toLowerCase(Locale.getDefault());
+                searchForMatch(text);
+            }
+        });
+
+    }
+
+    private  void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for a match: " +keyword);
+        mUserList.clear();
+
+        //update the users list
+        if (keyword.length() == 0){
+            mUserList.clear();
+        }else {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username)).equalTo(keyword);
+
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: found user: " +singleSnapshot.getValue(User.class).toString());
+
+                        mUserList.add(singleSnapshot.getValue(User.class));
+                        //update user list view
+                        updateUserList();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void updateUserList(){
+        Log.d(TAG, "updateUserList: updating users list ");
+
+        mAdapter = new UserListAdapter(SearchActivity.this, R.layout.layout_user_listitem, mUserList);
+
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: "+mUserList.get(position).toString());
+
+                //navigate to profile activity
+                Intent intent = new Intent(SearchActivity.this, ProfileActivity.class);
+                intent.putExtra(getString(R.string.calling_activity), getString(R.string.search_activity));
+                intent.putExtra(getString(R.string.intent_user), mUserList.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void hideSoftKeyboard(){
+
+        if (getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }
+
     }
 
     /**
